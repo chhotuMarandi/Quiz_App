4057b24bb77798cccabc10648413884f38b3004d
chhotu marandi <cmarandi29@gmail.com>
2023-05-30 01:16:36 +0530

    add js file

---
+++ app.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    question:
+      "Which programs reduce the size of the files so that they occupy lesser space on the disk?",
+    a: "Backup",
+    b: "Uninstall Program",
+    c: "File Compression",
+    d: "Disk Cleanup",
+  },
+  {
+    id: 2,
+    question: " Type of software that can be described as “end-user” software",
+    a: " DOS",
+    b: " application software",
+    c: "system software",
+    d: " operation software",
+  },
+  {
+    id: 3,
+    question:
+      "Which of these operating system does not have a graphical user interface ?",
+    a: "Windows 95",
+    b: "Mac OS",
+    c: "Linux",
+    d: "Ms Dos",
+  },
+];
